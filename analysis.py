e0d97c6273751668ea0d5b1b9d22b229270f52ea
EricZizhouWang <eric.zizhou.w@gmail.com>
2015-03-29 14:13:23 -0400

    -Implemented task4 analysis

--- analysis.py
+++ analysis.py
@@ -1,3 +1,4 @@
+import math
 task1_result = {"stylus":[], "mouse":[]}
 task2_part1_result = {"stylus":{"time":[], "error":[]}, "mouse":{"time":[], "error":[]}}
 task2_part2_result = {"stylus":{"time":[], "error":[]}, "mouse":{"time":[], "error":[]}}
@@ -5,7 +6,9 @@ task2_part3_result = {"stylus":{"time":[], "error":[]}, "mouse":{"time":[], "err
 task2_part4_result = {"stylus":{"time":[], "error":[]}, "mouse":{"time":[], "error":[]}}
 task2_part5_result = {"stylus":{"time":[], "error":[]}, "mouse":{"time":[], "error":[]}}
 
-task4_result = {"stylus":[], "mouse":[]}
+task4_part1_result = {"stylus":{"time":[], "error":[]}, "mouse":{"time":[], "error":[]}}
+task4_part2_result = {"stylus":{"time":[], "error":[]}, "mouse":{"time":[], "error":[]}}
+task4_part3_result = {"stylus":{"time":[], "error":[]}, "mouse":{"time":[], "error":[]}}
 
     
 
@@ -15,6 +18,8 @@ def analyze(file):
         if "Start tracking" in line:
             if "task1a" in line:
                 file = task1(file, "stylus")
+            elif "task1b" in line:
+                file = task1(file, "mouse")
             elif "task2a" in line:
                 if "Part 1" in line:
                     file = task2_part1(file, "stylus")
@@ -26,8 +31,6 @@ def analyze(file):
                     file = task2_part4(file, "stylus")
                 elif "Part 5" in line:
                     file = task2_part5(file, "stylus")
-            elif "task1b" in line:
-                file = task1(file, "mouse")
             elif "task2b" in line:
                 if "Part 1" in line:
                     file = task2_part1(file, "mouse")
@@ -39,29 +42,87 @@ def analyze(file):
                     file = task2_part4(file, "mouse")
                 elif "Part 5" in line:
                     file = task2_part5(file, "mouse")
+            elif "task4a" in line:
+                if "Part 1" in line:
+                    file = task4_part1(file, "stylus")
+                elif "Part 2" in line:
+                    file = task4_part2(file, "stylus")
+                elif "Part 3" in line:
+                    file = task4_part3(file, "stylus") 
+            elif "task4b" in line:
+                if "Part 1" in line:
+                    file = task4_part1(file, "mouse")
+                elif "Part 2" in line:
+                    file = task4_part2(file, "mouse")
+                elif "Part 3" in line:
+                    file = task4_part3(file, "mouse")
+                
         line = file.readline()
-            
-         
+    
+def distance_from_origin(x, y):
+    return math.sqrt((x**2 + y**2))
+    
+def task4(file, result, input, radius):
+    
+    line = file.readline()
+    while not "Timestamp" in line:
+        line = file.readline()
+        
+    init_x = int(line.split(" ")[3][2:-1])
+    init_y = int(line.split(" ")[4][2:])
+    start_time = int(line.split(" ")[2][:-1])
+    origin = (init_x + radius, init_y)
+    total_error = 0
+    
+    while (not "Stop tracking" in line) and ("task4" in line):
+        if "Timestamp" in line:
+            x = int(line.split(" ")[3][2:-1])
+            y = int(line.split(" ")[4][2:])
+    
+            drawn_radius = int(distance_from_origin(origin[0] - x, y - origin[1]))
+    
+            total_error += abs(drawn_radius - radius)
+                
+            end_time = int(line.split(" ")[2][:-1])
+        line = file.readline()
+        
+    total_time = end_time - start_time
+    result[input]["time"].append(total_time)
+    result[input]["error"].append(int(total_error))
+    
+    return file
+    
+
+
+def task4_part1(file, input):
+    return task4(file, task4_part1_result, input, 120)
+    
+def task4_part2(file, input):
+    return task4(file, task4_part2_result, input, 50)
+    
+def task4_part3(file, input):
+    return task4(file, task4_part3_result, input, 120)
+
 def task2(file, result, input, start, end):
     equation = line_solver(start[0], start[1], end[0], end[1]) 
     line = file.readline()
     start_time = int(line.split(" ")[2][:-1])
     total_error = 0
 
-    while not "Stop tracking" in line:
-        split_line = line.split(" ")
-        x = int(split_line[3][2:-1])
-        y = int(split_line[4][2:])
-        
-        y_actual = equation["m"]*x + equation["b"]
-        y_error = abs(y_actual - y)
-        total_error += y_error
-        end_time = int(line.split(" ")[2][:-1])
+    while (not "Stop tracking" in line) and ("task2" in line):
+        if "Timestamp" in line:
+            split_line = line.split(" ")
+            x = int(split_line[3][2:-1])
+            y = int(split_line[4][2:])
+            
+            y_actual = equation["m"]*x + equation["b"]
+            total_error += abs(y_actual - y)
+            end_time = int(line.split(" ")[2][:-1])
         line = file.readline()
         
     total_time = end_time - start_time
-    result[input]["time"] = total_time
-    result[input]["error"] = int(total_error)
+    result[input]["time"].append(total_time)
+    result[input]["error"].append(int(total_error))
     
     return file
    
@@ -91,21 +152,21 @@ def task1(file, input):
     split_line = line.split(" ")
     start_time = int(split_line[2][:-1])
     line = file.readline()
-    count = 2
     
-    while count < 5:
+    while (not "Stop tracking" in line) and ("task1" in line):
+        if "Timestamp" in line:
+            end_time = int(line.split(" ")[2][:-1])
         line = file.readline()
-        split_line = line.split(" ")
-        end_time = int(split_line[2][:-1])
-        line = file.readline()
-        count += 1
     task1_result[input].append(end_time - start_time)
     return file
     
     
 if __name__ == "__main__":
-    f = open('ex3.txt', 'r')
-    analyze(f)
+    files = ['ex1.txt','ex2.txt','ex3.txt','ex4.txt','ex5.txt','ex6.txt','ex7.txt','ex8.txt','ex9.txt']
+    for f_name in files:
+        f = open(f_name, 'r')
+        analyze(f)
+    
     
     print "task1:", task1_result,'\n'
     print "task2.1:", task2_part1_result,'\n'
@@ -113,4 +174,7 @@ if __name__ == "__main__":
     print "task2.3:", task2_part3_result,'\n'
     print "task2.4:", task2_part4_result,'\n'
     print "task2.5:", task2_part5_result,'\n'
+    print "task4.1:", task4_part1_result,'\n'
+    print "task4.2:", task4_part2_result,'\n'
+    print "task4.3:", task4_part3_result,'\n'
     
